a217c5f6b24d50af963e7f71d322920f3aa65f7f
Thomas Shetler <tommyaasum22@gmail.com>
2025-06-28 16:00:45 -0700

    Updated the Wiki with definition from JGMs Art and Pratice of Geomancy

--- GeomancyApp/FigureWiki.cs
+++ GeomancyApp/FigureWiki.cs
@@ -20,7 +20,6 @@ namespace GeomancyApp
 
         private void label1_Click(object sender, EventArgs e)
         {
-
             if (label1.Text == "  ◆")
             {
                 label1.Text = "◆ ◆";
@@ -90,23 +89,36 @@ namespace GeomancyApp
             FigureData FigFill = figureData.ReturnFigureData(FigureName(Convert.ToInt32(label1.Tag), Convert.ToInt32(label3.Tag), Convert.ToInt32(label4.Tag), Convert.ToInt32(label5.Tag)));
             UpdateText(FigFill);
         }
+
         public void UpdateText(FigureData fig)
         {
             FigName.Text = fig.Name;
             OtherNames.Text = fig.OtherNames;
             InnerElement.Text = fig.InnerEl;
-            OutterElement.Text = fig.OutterEl;
-            Quality.Text = fig.Quailty;
+            OutterElement.Text = fig.OuterEl;
+            Quality.Text = fig.Quality;
             Planet.Text = fig.Planet;
             Sign.Text = fig.Sign;
             Keyword.Text = fig.Keyword;
-            DivinaryMeaning.Text = fig.DivinaryMeaning;
+            DivinaryMeaning.Text = fig.DivinatoryMeaning;
             Imagery.Text = fig.Imagery;
-            
+            StrongHouse.Text = fig.StrongHouse;
+            WeakHouse.Text = fig.WeakHouse;
+
+            if (FireElement != null) FireElement.Text = fig.FireElement;
+            if (AirElement != null) AirElement.Text = fig.AirElement;
+            if (WaterElement != null) WaterElement.Text = fig.WaterElement;
+            if (EarthElement != null) EarthElement.Text = fig.EarthElement;
+
+            if (Anatomy != null) Anatomy.Text = fig.Anatomy;
+            if (BodyType != null) BodyType.Text = fig.BodyType;
+            if (CharacterType != null) CharacterType.Text = fig.CharacterType;
+            if (Colors != null) Colors.Text = fig.Colors;
+            if (Commentary != null) Commentary.Text = fig.Commentary;
         }
+
         public string FigureName(int h1, int h2, int h3, int h4)
         {
-
             if (h1 == 1 && h2 == 1 && h3 == 1 && h4 == 1)
             {
                 return "Via";
@@ -137,7 +149,7 @@ namespace GeomancyApp
             }
             else if (h1 == 2 && h2 == 2 && h3 == 2 && h4 == 1)
             {
-                return "Trisititia";
+                return "Tristitia";
             }
             else if (h1 == 1 && h2 == 2 && h3 == 2 && h4 == 1)
             {
@@ -184,6 +196,86 @@ namespace GeomancyApp
         }
 
         private void FigureWiki_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Quality_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Keyword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Planet_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sign_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StrongHouse_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WeakHouse_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InnerElement_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FireElement_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EarthElement_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WaterElement_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AirElement_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
         {
 
         }
